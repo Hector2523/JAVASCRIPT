066b78e3b0cfc4c688171d53bf995e7b04341d98
Hector2523 <67517312+Hector2523@users.noreply.github.com>
2023-08-27 17:30:06 -0300

    ym

---
+++ ex005/js/index.js
@@ -0,0 +1,27 @@
+//var nome, sobrenome, nomeCompleto, idade, pessoa;
+
+//nome = "Dimitri";
+
+//sobrenome = "Teixeira";
+
+//nomeCompleto = nome + " " + sobrenome;
+
+//idade = 30;
+
+//pessoa = "Idade:" + " " + idade + 10 + " " + "Nome Completo:" + nomeCompleto
+
+//alert(nomeCompleto);
+
+//document.getElementById("texto").innerHTML = (pessoa);
+
+
+
+
+
+const x=10;
+
+{
+    let x = 2;
+}
+
+document.getElementById("texto").innerHTML = x;
---
+++ lojinha/js/index.js
@@ -0,0 +1,8 @@
+
+//var valor1, valor2, total;
+
+//valor1 = 5;
+//valor2 = 2;
+//total = ++valor1;
+//alert(total);
+//alert("O valor 1 ficou: "+ valor1)
