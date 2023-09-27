2ac10f09e739176d1303c23b60544461a4116847
Hector2523 <67517312+Hector2523@users.noreply.github.com>
2023-09-27 17:52:21 -0300

    iej]

---
+++ 1btc/do-while/script/script.js
@@ -0,0 +1,8 @@
+let velocidade = 0;
+
+do {
+    alert("A celocidade do veículo é: " + velocidade + "Km/h");
+    velocidade -= 20;
+} while (false)
+
+alert("Velocidade final: " + velocidade + "Km/h")
