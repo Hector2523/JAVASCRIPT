a23141615a2f3143bbad8928b0eadec5305e8fe2
Hector2523 <67517312+Hector2523@users.noreply.github.com>
2023-10-01 17:41:47 -0300

    jgv

---
+++ 1btc/ex006-tabuada/script/script.js
@@ -0,0 +1,9 @@
+const numero = prompt("Olá eu sou um robô da tabuada!" + "\nInforme o número que você deseja calcular a tábuada");
+
+let resultado = "";
+
+for (let fator= 1; fator <= 20; fator++) {
+    resultado += " -> " + numero +" * " + fator + " = " + (numero*fator) + "\n";
+}
+
+alert("Resultado da tabuada de " + numero + " :\n\n" + resultado);
